d2f43bdbae1b6a69a9d1383d58fd5609e7e5ffbb
Richard Melo <allucardster@gmail.com>
2018-02-04 15:46:10 -0500

    symfony/web-server-bundle installed through symfony flex recipe

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
