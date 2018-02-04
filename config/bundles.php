248bec8d9413eb789b839c13cef35e8b0f232ff2
Richard Melo <allucardster@gmail.com>
2018-02-04 15:27:36 -0500

    api-platform/api-pack installed through symfony flex recipes

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,7 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
